df70201cf9442700db207cea0f6422f20a042dda
JCVACA <jvaca5@ucol.mx>
2025-10-28 09:46:13 -0600

    Initial Commit

---
+++ MuuPlay/MuuPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuuPlayApp.swift
+//  MuuPlay
+//
+//  Created by Julio César Vaca García on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MuuPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MuuPlayTests/MuuPlayTests.swift
@@ -0,0 +1,17 @@
+//
+//  MuuPlayTests.swift
+//  MuuPlayTests
+//
+//  Created by Julio César Vaca García on 28/10/25.
+//
+
+import Testing
+@testable import MuuPlay
+
+struct MuuPlayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
